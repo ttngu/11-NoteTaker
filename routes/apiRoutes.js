4cb6f19dd8cb3e4ce082dc92a0c459e8c21cc729
ttngu <tammyn137@gmail.com>
2020-12-04 00:55:16 -0800

    added code to read db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,25 @@
 // Dependencies
+const fs = require("fs");
 
 // Define const to read and wite files using promisify
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Module export function
+module.exports = function(app) {
+
     // Get API requests
+    app.get("/api/notes", function(req, res){
+
         // Read db.json file
+        readFileAsync("./db/db.json", "utf8").then(data => {
+
             // Return response as json object
+            res.json(db);
+        }).catch(err => {
+            if(err) throw err;
+        })
+    })
     
     // Post API requests
         // Read db.json file
@@ -21,3 +35,5 @@
     // Delete API requests
         // Read db.json file
         // Filter DB to exclude deleted, then rewrite db file
+
+}
