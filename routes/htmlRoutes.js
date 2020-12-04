451391d7b2932e43132160560bf4d997b6413161
ttngu <tammyn137@gmail.com>
2020-12-03 23:07:09 -0800

    add pseudo code to html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+// Dependencies
+const path = require("path");
+
+// Page routing
+module.exports = function(app) {
+    // Note page here to read, write and delete notes, add GET
+
+    // Home page here for other links, add GET
+    
+}
