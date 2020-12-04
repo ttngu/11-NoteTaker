4758b554a19b743110f613b72932cadffb29e827
ttngu <tammyn137@gmail.com>
2020-12-04 00:16:40 -0800

    added pseudo code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,23 @@
 // Dependencies
 
-// 
+// Define const to read and wite files using promisify
+
+// Module export function
+    // Get API requests
+        // Read db.json file
+            // Return response as json object
+    
+    // Post API requests
+        // Read db.json file
+        // console log
+        // Add id key to req.body
+
+        // Add Post info from req.body
+
+        // Write updated db to db.json
+            // Return response as true
+            // if err, throw err
+
+    // Delete API requests
+        // Read db.json file
+        // Filter DB to exclude deleted, then rewrite db file
