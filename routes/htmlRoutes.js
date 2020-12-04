6c175d431c4449b82542bcd8ea5981f997fc1810
ttngu <tammyn137@gmail.com>
2020-12-03 23:13:45 -0800

    added code for notes module export

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,9 @@ const path = require("path");
 // Page routing
 module.exports = function(app) {
     // Note page here to read, write and delete notes, add GET
-    
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/notes.html"));
+    });
     // Home page here for other links, add GET
     
 }
