49ab0675dc6e7e7c181bacd0fb9a1b7f8175c1ce
ttngu <tammyn137@gmail.com>
2020-12-03 23:09:00 -0800

    add public dir and html files inside

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Page routing
 module.exports = function(app) {
     // Note page here to read, write and delete notes, add GET
-
+    
     // Home page here for other links, add GET
     
 }
