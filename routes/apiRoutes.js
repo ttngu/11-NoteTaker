148776a63860b2df161ae2cc878633049aa18130
ttngu <tammyn137@gmail.com>
2020-12-08 01:09:34 -0800

    fully fixed delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,8 +14,6 @@ module.exports = function(app) {
 
     // Get API requests
     app.get("/api/notes", function(req, res){
-        // let db = readFileAsync(path.join(__dirname + "/../db/db.json"), "utf8");
-        // res.json(db);
         // Read db.json file
         return readFileAsync(path.join(__dirname + "/../db/db.json"), "utf8").then(data => {
             // Return response as json object
@@ -28,7 +26,6 @@ module.exports = function(app) {
     // Post API requests
     app.post("/api/notes", function(req, res){
         const { title, text } = req.body
-        console.log(req);
         console.log(req.body);
         id = uuidv1();
         const newData = {
@@ -46,61 +43,40 @@ module.exports = function(app) {
         }).catch(err => {
             if(err) throw err;
         })
-        // db.push(newData);
-        // // const db = fs.readFile("../db/db.json", (err) => err ? console.error(err) : console.log(db));
-        // // db.push(req);
-        // fs.writeFile(path.join(__dirname + "/../db/db.json"), JSON.stringify(db), (err) => err ? console.error(err) : console.log("Note added"));
-
-        // res.json(db);
-        
-        // Read db.json file
-        // readFileAsync("./db/db.json", "utf8").then(data => {
-
-        //     // console log
-        //     console.log(req.body);
-        //     // Add id key to req.body
-        //     req.body.id = uid();
-        //     const db = JSON.parse(data);
-
-        //     // Push post info from req.body
-        //     db.push(req.body);
-    
-        //     // Write updated db to db.json
-        //     writeFileAsync("./db/db.json", JSON.stringify(db)).then(() => {
-        //         // Return response as true
-        //         res.json(true)
-        //     });
-        // }).catch(err => {
-        //     if(err) throw err;
-        // })
     });
 
     
     // Delete API requests
     app.delete("/api/notes/:id", function(req, res){
         
-        // Read db.json file
-        // readFileAsync("./db/db.JSON", "utf8").then(data => {
-        //     const db = JSON.parse(data);
-
-        //     // Filter DB to exclude deleted, then rewrite db file
-        //     writeFileAsync("./db/db.json",JSON.stringify(db.filter(ele => {
-        //         console.log(ele.id);
-        //         console.log(req.params.id);
+        fs.readFile(path.join(__dirname + "/../db/db.json"), "utf8", (err, data) => {
+            if (err) {
+                console.log("File read failed:", err)
+                return
+            }
+            try {
+                const db = JSON.parse(data)
                 for (let i = 0; i < db.length; i++){
-                if (req.params.id === db[i].id){
-                    db.splice(i,1);
-                    console.log("This is where the data is spliced to remove the obj with the matching ID")}
+                    if (req.params.id === db[i].id){
+                        db.splice(i,1);
+                        console.log("This is where the data is spliced to remove the obj with the matching ID")
+                    }
                 }
-        // )))
-            
-            res.json(true);
-                
-            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db), function(err) {
-                if (err) throw err;
-            });
-        // }).catch(err => {
-        //     if(err) throw err;
-        // })
+                res.json(true);
+                    
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db), function(err) {
+                    if (err) throw err;
+                });
+            } catch(err) {
+                console.log('Error parsing JSON string:', err)
+            }
+        })
+
+
+
     });
+
 }
+
+
+
